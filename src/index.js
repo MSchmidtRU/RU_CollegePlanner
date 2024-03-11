ac9e55434618699bc0cbd6bd50f7b9245f329abf
Noa Strupinsky <noa.strupinsky@gmail.com>
2024-03-11 13:51:17 -0400

    clean up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+
 const { auth } = require('firebase/auth');
 const { firestore } = require('firebase/firestore');
 const { initializeApp } = require("firebase/app");
