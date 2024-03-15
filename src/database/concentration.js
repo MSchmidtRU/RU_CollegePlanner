a4a084fd15b7315762e4af892e1e10e7a15438e7
Hannah Bialik <hrb44@scarletmail.rutgers.edu>
2024-03-15 14:17:27 -0400

    modified:   src/database/concentration.js

--- src/database/concentration.js
+++ src/database/concentration.js
@@ -11,6 +11,72 @@ class Concentration {
     }
 }
 
+async function getConcentration(concentrationID)
+{
+    try {
+    const concentrationInfo = firestore.collection("concentration").doc(concentrationID);
+
+        // Retrieve the document data
+        const doc = await concentrationInfo.get();
+
+        if (doc.exists) {
+            // Document exists, access its data
+            const concentrationData = doc.data();
+
+            // Fetch basic student info
+            const name = concentrationData.name;
+            const residency = concentrationData.residency;
+
+            const coursesArray = concentrationData.courses || [];
+            const courses = await Helper.getAssociatedIDs(coursesArray);
+
+            const sampleScheudleArray = concentrationData.sample_schedule || [];
+            const sample_schedule = await Promise.all(sampleScheudleArray.map(async courseObj => {
+                const courseRef = courseObj.course;
+                const courseDoc = await courseRef.get();
+                if (courseDoc.exists) {
+                    return {
+                        id: courseDoc.id,
+                        semester: courseObj.semester,
+                        year: courseObj.year
+                    };
+                } else {
+                    console.log(`Course document ${courseRef.id} does not exist.`);
+                    return null;
+                }
+            }));
+            return new Concentration(name, courses, residency, sample_schedule);
+        }
+
+    }
+    catch (error) {
+        console.error('Error getting document:', error);
+        throw error;
+    }
+}
+
+async function insertConcentration(concentrationID, concentration) {
+    try {
+        if (!(concentration instanceof Concentration)) {
+            throw "course is not an instance of Course";
+        }
+
+        const concentrationData = {
+            name: concentration.name,
+            courses: Helper.createReference("courses", concentration.courses),
+            residency: concentration.residency,
+            sample_schedule: Helper.createReference("courses", concentration.sample_schedule)
+        }
+
+        const res = await firestore.collection('concentration').doc(concentrationID).set(concentrationData);
+    } catch (error) {
+        console.error('Error saving to Course document:', e);
+        throw e;
+    }
+}
+
+
+
 async function testing() {
     let concentraion = new Concentration("Software Engineering Finally", ["14:332:128", "14:332:221"], 51, ["14:332:128", "14:332:221"], [new FutureCourse("14:332:128", "Winter", 2025)]);
     await insertConcentration("111:222", concentraion);
