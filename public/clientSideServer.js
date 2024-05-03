73681cbb48927f4ff407061e77024ca2a5f54715
Noa Strupinsky <noa.strupinsky@gmail.com>
2024-05-02 20:31:57 -0400

    login wip

--- public/clientSideServer.js
+++ public/clientSideServer.js
@@ -7,12 +7,17 @@ const port = 8080;
 
 // Creating a server
 const server = http.createServer((req, res) => {
+    // Set CORS headers to allow requests from any origin
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
     // Handling requests for '/'
     if (req.url === '/') {
         // Reading the HTML file
         fs.readFile(path.join(__dirname, './login.html'), (err, data) => {
             if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain', });
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Internal Server Error');
             } else {
                 res.writeHead(200, { 'Content-Type': 'text/html' });
