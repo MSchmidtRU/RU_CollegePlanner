1f6dd50fcdff2d9ea8af19d36f697ba1fca7513e
Noa Strupinsky <noa.strupinsky@gmail.com>
2024-05-02 22:25:21 -0400

    google login works

--- public/clientSideServer.js
+++ public/clientSideServer.js
@@ -11,6 +11,8 @@ const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+    res.setHeader('Permissions-Policy', 'interest-cohort=()');
 
     // Handling requests for '/'
     if (req.url === '/') {
@@ -24,7 +26,35 @@ const server = http.createServer((req, res) => {
                 res.end(data);
             }
         });
-    } else {
+    }
+    else if (req.url === '/index') {
+        fs.readFile(path.join(__dirname, './index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    }
+    else if (req.url === '/tokensignin' && req.method === 'POST') {
+        // Handle the POST request to /tokensignin
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); // convert Buffer to string
+        });
+        req.on('end', () => {
+            // Parse the JSON data from the request body
+            const { idtoken } = JSON.parse(body);
+            // Here you can verify the token and perform necessary actions
+            // For now, let's just send a success response
+            console.log("HEre");
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('success');
+        });
+    }
+    else {
         // Handling other requests (like CSS, JavaScript files)
         const filePath = path.join(__dirname, req.url);
         fs.readFile(filePath, (err, data) => {
